1ef5b1ef30e04a9ea6a60d10ec04500267fde29c
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-02-07 11:13:26 -0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,21 +3,24 @@ pip3 install pyminifier
 pip3 install pyinstaller
 pip3 install -r requirements.txt
 mkdir pybuild
-rm pybuild/*
+del pybuild/*
 cp server.py pybuild/
 cp crystalchat.py pybuild/
 cp LICENSE pybuild/
 cp icon.ico pybuild/
+cp launcher.py pybuild/
 cd pybuild
-rm *_obf.py
-rm dist/*
+del *_obf.py
+del dist/*
 pyminifier --prepend=LICENSE -o crystalchat_obf.py crystalchat.py
+pyminifier --prepend=LICENSE -o launcher_obf.py launcher.py
 pyminifier --prepend=LICENSE -o server_obf.py server.py
 pyinstaller server_obf.py -i icon.ico --name CrystalChatServer --onefile
 pyinstaller crystalchat_obf.py -i icon.ico --name CrystalChat --onefile
+pyinstaller launhcer_obf.py -i icon.ico --name CrystalChatLauncher --onefile
 cd ..
 mkdir dist
-rm dist/*
+del dist/*
 cd pybuild
 cp dist/* ../dist/
 cd ..
