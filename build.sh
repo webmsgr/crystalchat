5c195a0d41890b7f42ee41a647653947dcb16d29
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-02-07 11:11:12 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,13 +7,16 @@ cp server.py pybuild/
 cp crystalchat.py pybuild/
 cp LICENSE pybuild/
 cp icon.ico pybuild/
+cp launcher.py pybuild/
 cd pybuild
 rm *_obf.py
 rm dist/*
 pyminifier --prepend=LICENSE -o crystalchat_obf.py crystalchat.py
+pyminifier --prepend=LICENSE -o launcher_obf.py launcher.py
 pyminifier --prepend=LICENSE -o server_obf.py server.py
 pyinstaller server_obf.py -i icon.ico --name CrystalChatServer --onefile
 pyinstaller crystalchat_obf.py -i icon.ico --name CrystalChat --onefile
+pyinstaller launhcer_obf.py -i icon.ico --name CrystalChatLauncher --onefile
 cd ..
 mkdir dist
 rm dist/*
