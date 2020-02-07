7edd11ada13a687516141eaf06830dcf997616c7
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-02-07 11:23:54 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,20 +3,14 @@ pip3 install -r requirements.txt
 pip3 install pyinstaller
 mkdir pybuild
 rm pybuild/*
-cp server.py pybuild/
-cp crystalchat.py pybuild/
+cp crystalchat.py pybuild/cchat.py
 cp LICENSE pybuild/
 cp icon.ico pybuild/
 cp launcher.py pybuild/
 cd pybuild
-rm *_obf.py
-rm dist/*
-pyminifier --prepend=LICENSE -o crystalchat_obf.py crystalchat.py
+pyminifier --prepend=LICENSE -o crystalchat.py cchat.py
 pyminifier --prepend=LICENSE -o launcher_obf.py launcher.py
-pyminifier --prepend=LICENSE -o server_obf.py server.py
-pyinstaller server_obf.py -i icon.ico --name CrystalChatServer --onefile
-pyinstaller crystalchat_obf.py -i icon.ico --name CrystalChat --onefile
-pyinstaller launhcer_obf.py -i icon.ico --name CrystalChatLauncher --onefile
+pyinstaller launcher_obf.py -i icon.ico --name CrystalChat --onefile
 cd ..
 mkdir dist
 rm dist/*
