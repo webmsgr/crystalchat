7675a4c493a4809e5567fef6e4c192c72c5115b6
webmsgr <zapcontact000@gmail.com>
2020-02-07 06:30:33 -0800

    linux build file

---
+++ build.sh
@@ -0,0 +1,21 @@
+pip3 install pyminifier
+pip3 install -r requirements.txt
+mkdir pybuild
+rm pybuild/*
+cp server.py pybuild/
+cp crystalchat.py pybuild/
+cp LICENSE pybuild/
+cp icon.ico pybuild/
+cd pybuild
+rm *_obf.py
+rm dist/*
+pyminifier --prepend=LICENSE -o crystalchat_obf.py crystalchat.py
+pyminifier --prepend=LICENSE -o server_obf.py server.py
+pyinstaller server_obf.py -i icon.ico --name CrystalChatServer --onefile
+pyinstaller crystalchat_obf.py -i icon.ico --name CrystalChat --onefile
+cd ..
+mkdir dist
+rm dist/*
+cd pybuild
+cp dist/* ../dist/
+cd ..
