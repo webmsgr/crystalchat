0a405a103fa00d91dafacdaee4cd8692d11973e9
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-02-07 09:15:03 -0800

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 import urllib.request
+import configparser
+import shutil
+import tempfile
+
+vurl = "https://raw.githubusercontent.com/webmsgr/crystalchat4/master/autoupdate/versions"
 def get_request(url):
-    with urllib.request.urlopen('http://python.org/') as response:
-        data = response.read()
-    return data
+    with urllib.request.urlopen(url) as response:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            shutil.copyfileobj(response, tmp_file)
+    return tmp_file
+
