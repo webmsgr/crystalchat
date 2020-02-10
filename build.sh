5e741d2c340afff549ea77565657f2420486b060
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-02-10 07:40:06 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ pip3 install pyminifier
 pip3 install -r requirements.txt
 pip3 install pyinstaller
 pyminifier --prepend=LICENSE -o c.py crystalchat.py
-pyinstaller --add-data "versions;autoupdate" c.py -i icon.ico --name CrystalChat --onefile
+pyinstaller --add-data "versions:autoupdate" c.py -i icon.ico --name CrystalChat --onefile
 rm c.py
