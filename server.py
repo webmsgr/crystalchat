4964a4d5a5f55f1718d95ceb78cf1f796290c53e
Wackery <32279125+webmsgr@users.noreply.github.com>
2020-02-12 11:32:49 -0800

    Update server.py (#10)
    * Update server.py
    
    * Restyle Update server.py (#11)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,39 +1,79 @@
 import asyncio
 import hashlib
 import json
-from crystalcommon import MessageTypes
-import websockets
 import multiprocessing as mp
 import queue
+
+import websockets
+
+from crystalcommon import MessageTypes
+
 clients = []
 messages = []
 
 
+class Client:
+    def __init__(self, nickname, serverman):
+        self.nickname = nickname
+        self.server = server
+        self.cid = None
+        self.connected = False
+
+    def ready(self):
+        if self.connected:
+            raise RuntimeError("Client already registered")
+        self.cid = self.server.add_client(self)
+        self.connected = True
+
+    def leave(self):
+        if not self.connected:
+            raise RuntimeError("Client is not registered")
+        self.server.remove_client(self.cid)
+        self.cid = None
+        self.connected = False
+
+    def on_message(self, msg):
+        if self.connected:
+            self.server.on_message(self.cid, msg)
+        else:
+            raise RuntimeError("Client is not registered")
+
+    def get_messages(self):
+        if self.connected:
+            return self.server.get_messages(self.cid)
+        else:
+            raise RuntimeError("Client is not registered")
+
+
 class TheServer:
-    """Manages clients and crap, routes messages, etc."""
+    """Manages clients, routes messages, etc."""
+
     def __init__(self):
         self.clients = []
         self.clientqueues = []
         self.messagequeue = mp.Queue()
         self.clientlock = mp.Lock()
-    def add_client(self,client): # Called on client join
+
+    def add_client(self, client):  # Called on client join
         self.clientlock.acquire()
         if None in self.clients:
             clid = self.clients.index(None)
             self.clients[clid] = client
             self.clientqueues[clid] = mp.Queue()
-            
+
         else:
             clid = len(self.clients)
             self.clients.append(client)
             self.clientqueues.append(mp.Queue())
-        self.clientlock.release()  
+        self.clientlock.release()
         return self.clid
-    def removeclient(self,cid): # called on client leave
+
+    def remove_client(self, cid):  # called on client leave
         self.clientlock.acquire()
         self.clients[cid] = None
         self.clientlock.release()
-    def get_messages(self,cid): # Called by each websocket client
+
+    def get_messages(self, cid):  # Called by each websocket client
         queuetoget = self.clientqueues[cid]
         mess = []
         while True:
@@ -42,22 +82,54 @@ class TheServer:
             except queue.Empty:
                 break
         return mess
-    def send_message(self,cid,message,to):
-        message = (cid,message)
+
+    def send_message(self, cid, message, to):
+        message = (cid, message)
+        if to == "*":
+            for num, client in enumerate(self.clients):
+                if client:
+                    self.clientqueues[num].put(message)
         for client in to:
             if self.clients[client]:
                 self.clientqueues[client].put(message)
-    def on_message(self,cid,raw_message): # called by websocket clients
-        self.messagequeue.put((cid,raw_message))
+
+    def on_message(self, cid, raw_message):  # called by websocket clients
+        self.messagequeue.put((cid, raw_message))
+
     def process_msg(self):
         msg = self.messagequeue.get()
         cid = msg[0]
-        msg = self._process(msg[1])
-        self.send_message(cid,msg,[])
-    def _process(self,msg):
+        msg = self._process(cid, msg[1])
+        self.send_message(cid, msg, "*")
+
+    def _process(self, cid, msg):  # Override in subclass
         return msg
-    def start_server(self): # Starts a websocket server in another thread.
-        raise NotImplemented
+
+
+def server(sobj, hostname, port):
+    eventloop = asyncio.new_event_loop()
+    asyncio.set_event_loop(eventloop)
+
+    async def websocketserver(websocket, path):
+        async for message in websocket:
+            # Simple echo server until i implement the real thing
+            await websocket.send(message)
+
+    # start a websocket server
+    start_server = websockets.serve(websocketserver, hostname, port)
+    eventloop.run_until_complete(start_server)
+    eventloop.run_forever()
+
+
+def start_server(hostname, port, sclass=TheServer):
+    serverobj = sclass()
+    sproc = mp.Process(target=server,
+                       args=(serverobj, hostname, port),
+                       daemon=True)
+    sproc.start()
+    sproc.join()
+
+
 async def hello(websocket, path):
     print(path)
     myid = hashlib.new("md5")
@@ -93,7 +165,8 @@ async def hello(websocket, path):
         await client.send(myid + b" has left")
 
 
-start_server = websockets.serve(hello, "localhost", 42069)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+if __name__ == "__main__":
+    mp.freeze_support()
+    start_server = websockets.serve(hello, "localhost", 42069)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
